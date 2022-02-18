7700105c717bf228ed777747a73b1c9a53cfccfe
SalihKalender <m.salihk72@gmail.com>
2022-02-18 16:18:29 +0300

    initial commit1

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const graphql = require('graphql')
+const fse = require('fs-extra')
+const path = require('path')
+const { GraphQLObjectType } = graphql
+const { graphqlHTTP } = require('express-graphql')
+
+const app = express();
+
+const Post_data = require('./fake_data/posts.json')
+
+const Post_Type = new GraphQLObjectType({
+    name: 'Post',
+    fields: {
+        userId: { type: graphql.GraphQLInt },
+        id: { type: graphql.GraphQLInt },
+        title: { type: graphql.GraphQLString },
+        body: { type: graphql.GraphQLString },
+    }
+})
+
+const QueryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        getPost: {
+            type: Post_Type,
+            args: {
+                id: {
+                    type: graphql.GraphQLInt
+                }
+            },
+            resolve: (_, { id }) => {     /* yukarıda belirttigimiz id */
+                return Post_data.find(post => post.id == id)
+            }
+        },
+        getAllPosts: {
+            type: new graphql.GraphQLList(Post_Type),
+            args: {
+                id: {
+                    type: graphql.GraphQLInt
+                }
+            },
+            resolve: (_, { id }) => {     /* yukarıda belirttigimiz id */
+                return Post_data
+            }
+        }
+    }
+})
+
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+      addPost: {
+        type: Post_Type,
+        args: {
+            title: { type: graphql.GraphQLString },
+            body: { type: graphql.GraphQLString },
+        },
+        resolve(parent, args) {
+            Post_data.push({
+                id: Post_data.length + 1,
+                userId: 4,
+                title: args.title,
+                body: args.body,
+            });
+            fse.outputFile(path.join(__dirname, '/fake_data/posts.json'), JSON.stringify(Post_data))
+            return {...args, id: Post_data[Post_data.length - 1].id, userId: Post_data[Post_data.length - 1].userId};
+        },
+      },
+    },
+  });
+
+
+const Schema = new graphql.GraphQLSchema({ query:  QueryType, mutation: Mutation})
+
+app.use('/graphql', graphqlHTTP({
+    schema: Schema,
+    graphiql: true
+}))
+
+app.listen(6969, () => {
+    console.log('Running a GraphQL API server at localhost:6969/graphql')
+})
+
+
